36d6efea6698c9aa3edd34947f51c3a095225751
Andrew Wong <ahw.inbox@gmail.com>
2021-01-13 20:42:40 -0800

    Add kyria keymap

---
+++ keyboards/kyria/keymaps/lettucedeep/rules.mk
@@ -0,0 +1,30 @@
+OLED_DRIVER_ENABLE = yes   # Enables the use of OLED displays
+ENCODER_ENABLE = no        # Enables the use of one or more encoders
+RGBLIGHT_ENABLE = no       # Enable keyboard RGB underglow
+RGBLIGHT_ANIMATIONS = no
+
+BOOTMAGIC_ENABLE = no       # Virtual DIP switch configuration
+MOUSEKEY_ENABLE = yes       # Mouse keys
+EXTRAKEY_ENABLE = yes       # Audio control and System control
+CONSOLE_ENABLE = no         # Console for debug
+COMMAND_ENABLE = no         # Commands for debug and configuration
+NKRO_ENABLE = yes           # Nkey Rollover - if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
+BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
+MIDI_ENABLE = no            # MIDI controls
+AUDIO_ENABLE = no           # Audio output on port C6
+UNICODE_ENABLE = no         # Unicode
+BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
+SWAP_HANDS_ENABLE = no      # Enable one-hand typing
+WPM_ENABLE = yes            # supplies get_current_wpm function for bongocat
+
+# added from j-inc
+TAP_DANCE_ENABLE = no
+STENO_ENABLE = no
+TERMINAL_ENABLE = no
+GRAVE_ESC_ENABLE = no
+MAGIC_ENABLE = no
+SPACE_CADET_ENABLE = no
+KEY_LOCK_ENABLE = no
+AUDIO_ENABLE = no
+CONSOLE_ENABLE = no
+VELOCIKEY_ENABLE = no
