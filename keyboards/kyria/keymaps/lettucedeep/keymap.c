5520ac5ede23bb53b55a57043eb02681b8cb388d
Andrew Wong <ahw.inbox@gmail.com>
2021-06-02 18:21:53 -0700

    up kyria f-row to match number layer f-row

--- keyboards/kyria/keymaps/lettucedeep/keymap.c
+++ keyboards/kyria/keymaps/lettucedeep/keymap.c
@@ -150,7 +150,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      [_SYMBOL] = LAYOUT(
        _______, _______, _______, KC_UNDS, KC_MINS, _______,                                     _______, KC_PLUS, KC_EQL , _______, _______, _______,
        KC_GRV , KC_EXLM, KC_AT  , KC_HASH, KC_DLR , KC_PERC,                                     KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_MINS,
-       _______, _______, KC_LBRC, KC_LCBR, KC_LPRN, _______, _______, _______, _______, _______, _______, KC_RPRN, KC_RCBR, KC_RBRC, _______, _______,
+       _______, KC_LABK, KC_LBRC, KC_LCBR, KC_LPRN, _______, _______, _______, _______, _______, _______, KC_RPRN, KC_RCBR, KC_RBRC, KC_RABK, _______,
                                   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
      ),
 /*
