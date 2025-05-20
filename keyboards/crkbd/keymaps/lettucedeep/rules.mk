50d8dc4122153a545fbc3c46a21a577a4bb89d06
awong <qtipconnoisseur@gmail.com>
2025-05-19 17:30:48 -0700

    update crkbd keymap/rules.

--- keyboards/crkbd/keymaps/lettucedeep/rules.mk
+++ keyboards/crkbd/keymaps/lettucedeep/rules.mk
@@ -2,5 +2,5 @@ BOOTLOADER      = atmel-dfu
 MOUSEKEY_ENABLE = yes    # Mouse keys
 RGBLIGHT_ENABLE = no     # Enable WS2812 RGB underlight.
 OLED_ENABLE     = yes
-OLED_DRIVER     = SSD1306
+OLED_DRIVER     = ssd1306
 LTO_ENABLE      = yes
